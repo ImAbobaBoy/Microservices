173e4150569f9632138d065b45cb62858b62e35f
ImAbobaBoy <sysoev.ae705@gmail.com>
2025-11-17 16:49:54 +0500

    sixthTask

---
+++ Services/TeamManagerApi/Logic/Services/SemaphoreService/Interfaces/ISemaphoreService.cs
@@ -0,0 +1,14 @@
+﻿using DistributedLocking.Interfaces;
+
+namespace Logic.Services.SemaphoreService.Interfaces;
+
+/// <summary>
+/// Сервис для сефамора
+/// </summary>
+public interface ISemaphoreService
+{
+    /// <summary>
+    /// Получить семафор
+    /// </summary>
+    IDistributedSemaphore GetSemaphore();
+}
